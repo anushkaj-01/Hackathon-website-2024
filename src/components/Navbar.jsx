ac0d7aafa4ff6f141abf43a0346672f9f702b706
Anushka Jhingran <anushkajhingran@gmail.com>
2024-05-11 23:50:15 +0530

    Update Navbar.jsx
    fixed navbar image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar(){
     return(
         <>
         <nav className="flex bg-gradient-to-b from-[#040842] to-[#040842] justify-between w-[100vw] h-[100px] mt-0 bg-blue-800 font-vt323">
-            <img className="ml-[2%] pt-[1%]" src="./public/images/logo.svg"></img>
+            <img className="ml-[2%] pt-[1%]" src="./images/logo.svg"></img>
             
             <ul className="flex space-x-[3rem] text-[2rem] text-white items-center">
                 <a onClick={scrollToSchedule} style={{ cursor: 'pointer' }}><li>SCHEDULE</li></a>
@@ -22,7 +22,7 @@ export default function Navbar(){
                 <li style={{ cursor: 'pointer' }}>PRIZES</li>
                 <a onClick={scrollToFooter} style={{ cursor: 'pointer' }}><li>MORE</li></a>
             </ul>
-            <img className="mr-[3%] pt-[1%]" src="./public/images/IIITlogo.svg"></img>
+            <img className="mr-[3%] pt-[1%]" src="./images/IIITlogo.svg"></img>
             
         </nav>
         </>
